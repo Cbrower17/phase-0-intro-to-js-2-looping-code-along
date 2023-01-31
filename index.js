7ba435577b258416f1cddc417e064ff779d46751
Christopher <chris.brower.96@gmail.com>
2023-01-31 11:19:42 -0700

    pwned lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+function writeCards(names, giftType){
+    const messages = [];
+ for(let index = 0;index< names.length; index++){
+    console.log(names);
+    console.log("Thank you, " + names[index] + " for the wonderful " + giftType + " gift!");
+    messages.push(`Thank you, ${names[index]}, for the wonderful ${giftType} gift!`)
+ }
+ return messages;
+}
+
+function countDown(){
+
+    let count = 10;
+
+    while (count >= 0) {
+        console.log(count);
+        count--;
+    }
+}
